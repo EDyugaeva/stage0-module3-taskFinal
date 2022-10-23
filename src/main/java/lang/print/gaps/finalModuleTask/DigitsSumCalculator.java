8466af76b304ebd312ce34f78a27570f353c5b32
EDyugaeva <ed2408@yandex.ru>
2022-10-23 20:08:48 +0300

    commit

--- src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
+++ src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
@@ -2,5 +2,16 @@ package lang.print.gaps.finalModuleTask;
 
 public class DigitsSumCalculator {
     public void calculateSum(int number) {
+        int a = number % 10; // 4
+        int b = number % 100 / 10; // 3
+        int c = number / 100 % 10; // 2
+        int d = number / 1000; // 1
+        System.out.println(a + b+ c+d);
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println(1234 / 100 %10);
     }
 }
+
--- src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
@@ -1,6 +1,15 @@
 package lang.print.gaps.finalModuleTask;
 
 public class NumberReverter {
-    public void revert(int number) {
+    static public void revert(int number) {
+        int c = number % 10;
+        int b = number % 100 / 10;
+        int a = number / 100;
+        System.out.println(c * 100 + b * 10 + a );
+
+    }
+
+    public static void main(String[] args) {
+        revert(334);
     }
 }
